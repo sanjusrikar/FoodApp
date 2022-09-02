dc991a1efa5251466607bf700d25dfa8d8216669
Sanjay Srikar Thamma <sanjaysrikar.thamma@clarivate.com>
2022-09-02 13:39:22 +0530

    folder refactored

--- FoodApp/FoodApp/src/main/java/com/cl/FoodApp/repository/FoodOrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cl.FoodApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cl.FoodApp.dto.FoodOrder;
-
-public interface FoodOrderRepo extends JpaRepository<FoodOrder,Integer>{
-
-}
--- FoodApp/FoodApp/src/main/java/com/cl/FoodApp/repository/MenuRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cl.FoodApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cl.FoodApp.dto.Menu;
-
-public interface MenuRepo extends JpaRepository<Menu,Integer>{
-
-}
